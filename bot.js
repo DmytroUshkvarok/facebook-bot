7d9a075de42eecb05bf24f0e5c844430a8501ef6
Dmytro Ushkvarok <ushkvarokdmitriy@gmail.com>
2019-01-20 15:52:35 +0200

    first commit

---
+++ bot.js
@@ -0,0 +1,65 @@
+var env = require('node-env-file');
+env(__dirname + '/.env');
+
+var winston = require('winston');
+
+var options = {
+    file: {
+      level: 'info',
+      name: 'file.info',
+      filename: `./logs/app.log`,
+      handleExceptions: true,
+      json: true,
+      maxsize: 5242880, // 5MB
+      maxFiles: 100,
+      colorize: true,
+    },
+    errorFile: {
+      level: 'error',
+      name: 'file.error',
+      filename: `./logs/error.log`,
+      handleExceptions: true,
+      json: true,
+      maxsize: 5242880, // 5MB
+      maxFiles: 100,
+      colorize: true,
+    },
+    console: {
+      level: 'debug',
+      handleExceptions: true,
+      json: false,
+      colorize: true,
+    },
+};
+
+
+var Botkit = require('botkit');
+var debug = require('debug')('botkit:main');
+
+var controller = Botkit.facebookbot({
+    verify_token: process.env.verify_token,
+    access_token: process.env.page_token,
+    stats_optout: true,
+    debug: true,
+    logger: winston.createLogger({
+        transports: [
+          new (winston.transports.Console)(options.console),
+          new (winston.transports.File)(options.errorFile),
+          new (winston.transports.File)(options.file)
+        ],
+        exitOnError: false, // do not exit on handled exceptions
+      }),
+      require_delivery: true,
+      validate_requests: true
+});
+
+var webserver = require(__dirname + '/components/express_webserver.js')(controller);
+require(__dirname + '/components/subscribe_events.js')(controller);
+require(__dirname + '/components/facebook_settings.js')(controller);
+require(__dirname + '/components/skills.js')(controller);
+
+
+
+
+
+
---
+++ components/facebook_settings.js
@@ -0,0 +1,28 @@
+var debug = require('debug')('botkit:facebook_settings');
+
+
+
+module.exports = function(controller) {
+
+    debug('Configuring Facebook thread settings...');
+    controller.api.thread_settings.greeting('Look to Up and enjoy. More.');
+    controller.api.thread_settings.get_started('start_button_clicked');
+    controller.api.thread_settings.menu([
+        {
+            "locale": "default",
+            "composer_input_disabled": false,
+            "call_to_actions": [    
+                {
+                    "type":"postback",
+                    "title":"Main menu",
+                    "payload":"main_menu"
+                },
+                {
+                    "type":"postback",
+                    "title":"Catalog",
+                    "payload":"catalog"
+                }
+            ]
+        }
+    ]);
+}
