7b76ae9ac8a4ec3265100ac649596d7b6603ab93
Dmytro Ushkvarok <ushkvarokdmitriy@gmail.com>
2019-01-23 13:44:32 +0200

    push to develop

--- bot.js
+++ bot.js
@@ -1,68 +1,30 @@
-var env = require('node-env-file');
+const env = require('node-env-file');
 env(__dirname + '/.env');
 
-var winston = require('winston');
+const Botkit = require('botkit');
+const debug = require('debug')('botkit:main');
+const mongoUri = process.env.mongodb_uri;
+const winston = require('winston');
+const mongoStorage = require(__dirname + '/components/botkit_mongo_storage.js')({mongoUri: mongoUri});
 
-var options = {
-    file: {
-      level: 'info',
-      name: 'file.info',
-      filename: `./logs/app.log`,
-      handleExceptions: true,
-      json: true,
-      maxsize: 5242880, // 5MB
-      maxFiles: 100,
-      colorize: true,
-    },
-    errorFile: {
-      level: 'error',
-      name: 'file.error',
-      filename: `./logs/error.log`,
-      handleExceptions: true,
-      json: true,
-      maxsize: 5242880, // 5MB
-      maxFiles: 100,
-      colorize: true,
-    },
-    console: {
-      level: 'debug',
-      handleExceptions: true,
-      json: false,
-      colorize: true,
-    },
-};
-
-
-var Botkit = require('botkit');
-var debug = require('debug')('botkit:main');
-var mongoUri = process.env.mongodb_uri;
-var mongoStorage = require(__dirname + '/components/botkit_mongo_storage.js')({mongoUri: mongoUri});
-
-var controller = Botkit.facebookbot({
+const controller = Botkit.facebookbot({
     verify_token: process.env.verify_token,
     access_token: process.env.page_token,
     stats_optout: true,
     debug: true,
     logger: winston.createLogger({
-        transports: [
-          new (winston.transports.Console)(options.console),
-          new (winston.transports.File)(options.errorFile),
-          new (winston.transports.File)(options.file)
-        ],
-        exitOnError: false, // do not exit on handled exceptions
-      }),
+      levels: winston.config.syslog.levels,
+      transports: [
+        new (winston.transports.Console)({level: 'debug'}),
+        new (winston.transports.File)({ filename: './logs/bot.log' })
+      ]
+    }),
       require_delivery: true,
       validate_requests: true,
       storage: mongoStorage
 });
 
-var webserver = require(__dirname + '/components/express_webserver.js')(controller);
+const webserver = require(__dirname + '/components/express_webserver.js')(controller);
 require(__dirname + '/components/subscribe_events.js')(controller);
 require(__dirname + '/components/facebook_settings.js')(controller);
 require(__dirname + '/components/skills.js')(controller);
-
-
-
-
-
-
