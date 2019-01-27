961b71fbe1f56763e56cf7fca85f42a800153989
ushkvarokdmitriy@gmail.com <ushkvarokdmitriy@gmail.com>
2019-01-27 16:13:38 +0200

    shop mechanics aded

--- components/skills.js
+++ components/skills.js
@@ -8,45 +8,61 @@ module.exports = function(controller) {
     });
 
     const mainMenu = {
-        text: 'MAIN MENU',
-            quick_replies: 
+        'text': 'MAIN MENU',
+            'quick_replies': 
             [{
-              content_type: 'text',
-              title: 'My purchases',
-              payload: 'purchases',
+              'content_type': 'text',
+              'title': 'My purchases',
+              'payload': 'purchases',
             },
             {
-              content_type: 'text',
-              title: 'Shop',
-              payload: 'shop',
+              'content_type': 'text',
+              'title': 'Shop',
+              'payload': 'shop',
             },
             {
-              content_type: 'text',
-              title: 'Favourites',
-              payload: 'favourites',
+              'content_type': 'text',
+              'title': 'Favourites',
+              'payload': 'favourites',
             },
             {
-              content_type: 'text',
-              title: 'To invite a friend',
-              payload: 'invitation',
+              'content_type': 'text',
+              'title': 'To invite a friend',
+              'payload': 'invitation',
             }]
     };
 
     const backToMainMenuButton = {
-        content_type: 'text',
-        title: 'To main menu',
-        payload: 'main_menu',
+        'content_type': 'text',
+        'title': 'To main menu',
+        'payload': 'main_menu',
     };
-
-    const seeMoreProductsButton = {
-        content_type: 'text',
-        title: 'See more products',
-        payload: 'see_more_products',
+    
+    const showCatalogButton = {
+        'content_type': 'text',
+        'title': 'Back to catalog',
+        'payload': 'back_to_catalog',
     };
 
     function showMainMenu(bot, message) {
         bot.reply(message, mainMenu);
     }
+
+    controller.on('facebook_postback', function(bot, message) {
+
+        if (message.payload == 'start_button_clicked') {
+
+            showMainMenu(bot, message);
+        }
+    });
+
+    controller.on('facebook_postback', function(bot, message) {
+
+        if (message.payload == 'catalog') {
+
+            showingCatalog(bot, message);
+        }
+    });
 
     controller.on('facebook_postback, message_received', function(bot, message) {
 
@@ -59,65 +75,299 @@ module.exports = function(controller) {
 
             if (message.quick_reply.payload == 'shop') {
 
-                bby.products('search=headphones', {
+                showingCatalog(bot, message);
+            }
+
+            if (message.quick_reply.payload == 'back_to_catalog') {
+
+                showingCatalog(bot, message);
+            }
+            
+            if (~message.quick_reply.payload.indexOf('go_to_next_page')) {
+
+                const number = +message.quick_reply.payload.split(' ')[1] + 1;
+
+                showingCatalog(bot, message, number);
+            }
+            
+            if (~message.quick_reply.payload.indexOf('go_to_previous_page')) {
+
+                const number = +message.quick_reply.payload.split(' ')[1] - 1;
+
+                showingCatalog(bot, message, number);
+            }
+
+        } else if (message.payload == 'main_menu') {
+        
+            showMainMenu(bot, message);
+
+        } else if (typeof(message.payload) === 'string' && ~message.payload.indexOf('view_more')) {
+
+            const skuOfProduct = message.payload.slice(message.payload.indexOf(' ') + 1);
+            
+            bby.products(`search=${skuOfProduct}`, {
+                format: 'json',
+                show: 'name,image,longDescription,salePrice'
+              }, function (err, data) {
+                
+                if (err) {
+                    console.warn(err);
+                }
+                
+                const createProductDetailsAttachment = function(data) {
+                    
+                    const objectToCreate = {};
+                    objectToCreate.type = 'template';
+                    objectToCreate.payload = {};
+                    objectToCreate.payload.template_type = 'generic';
+                    objectToCreate.payload.elements = [];
+
+                    const productName = data.products[0].name;
+                    const productImageURL = data.products[0].image;
+                    const longProductDescription = data.products[0].longDescription;
+                    const productPrice = data.products[0].salePrice;
+
+                    objectToCreate.payload.elements[0] = {};
+                    objectToCreate.payload.elements[0].title = productName;
+                    objectToCreate.payload.elements[0].subtitle = longProductDescription;
+                    objectToCreate.payload.elements[0].image_url = productImageURL;
+                    objectToCreate.payload.elements[0].buttons = [
+                        {
+                            'type':'postback',
+                            'title':'Add to favourites',
+                            'payload':`add_to_favourites ${skuOfProduct}`,
+                        },
+                        {
+                            'type':'postback',
+                            'title':`Buy for ${productPrice} $`,
+                            'payload':`buy_product ${skuOfProduct}`,
+                        }
+                    ];
+                
+
+                    return objectToCreate;
+                };
+                
+                const productDetailsAttachment = createProductDetailsAttachment(data);
+
+                bot.reply(message, {
+                    'attachment': productDetailsAttachment,
+                    'quick_replies': [backToMainMenuButton, showCatalogButton]
+                });
+            });
+
+        } else if (typeof(message.payload) === 'string' && ~message.payload.indexOf('buy_product')) {
+
+            const skuOfProduct = message.payload.slice(message.payload.indexOf(' ') + 1);
+
+            bot.reply(message, {
+                'text': 'Please share your phone',
+                'quick_replies': [
+                    {
+                        'content_type': 'user_phone_number',                    
+                    },
+                    {                    
+                        'content_type':'text',
+                        'title':'Back to product',
+                        'payload':`back_to_product ${skuOfProduct}`,
+                    }
+                ]
+            });
+
+        }
+    });
+
+
+    controller.on('message_received', function(bot, message) {
+
+        if (message.quick_reply) {
+
+            if (~message.quick_reply.payload.indexOf('back_to_product')) {
+
+                const skuOfProduct = message.quick_reply.payload.slice(message.quick_reply.payload.indexOf(' ') + 1);
+            
+                bby.products(`search=${skuOfProduct}`, {
                     format: 'json',
-                    show: 'name,image,sku'
-                  }, function (err, data) {
+                    show: 'name,image,longDescription,salePrice'
+                }, function (err, data) {
                     
                     if (err) {
                         console.warn(err);
                     }
                     
-                    const createProductsListAttachment = function(data) {
+                    const createProductDetailsAttachment = function(data) {
                         
-                        let i;
-                        let objectToCreate = {};
+                        const objectToCreate = {};
                         objectToCreate.type = 'template';
                         objectToCreate.payload = {};
                         objectToCreate.payload.template_type = 'generic';
                         objectToCreate.payload.elements = [];
 
-                        for (i = 0; i < 1; i++) {
-                            
-                            const productName = data.products[i].name;
-                            const productImageURL = data.products[i].image;
-                            objectToCreate.payload.elements[i] = {};
-                            objectToCreate.payload.elements[i].title = productName;
-                            objectToCreate.payload.elements[i].image_url = productImageURL;
-                            objectToCreate.payload.elements[i].buttons = [
-                                {
-                                    'type':'postback',
-                                    'title':'View more',
-                                    'payload':`view_more ${data.products[i].sku}`,
-                                }
-                            ];
-                        }
+                        const productName = data.products[0].name;
+                        const productImageURL = data.products[0].image;
+                        const longProductDescription = data.products[0].longDescription;
+                        const productPrice = data.products[0].salePrice;
+
+                        objectToCreate.payload.elements[0] = {};
+                        objectToCreate.payload.elements[0].title = productName;
+                        objectToCreate.payload.elements[0].subtitle = longProductDescription;
+                        objectToCreate.payload.elements[0].image_url = productImageURL;
+                        objectToCreate.payload.elements[0].buttons = [
+                            {
+                                'type':'postback',
+                                'title':'Add to favourites',
+                                'payload':`add_to_favourites ${skuOfProduct}`,
+                            },
+                            {
+                                'type':'postback',
+                                'title':`Buy for ${productPrice} $`,
+                                'payload':`buy_product ${skuOfProduct}`,
+                            }
+                        ];
+                    
 
                         return objectToCreate;
                     };
                     
-                    const productsListAttachment = createProductsListAttachment(data);
-
+                    const productDetailsAttachment = createProductDetailsAttachment(data);
 
                     bot.reply(message, {
-                        "attachment": productsListAttachment,
-                        "quick_replies": [backToMainMenuButton, seeMoreProductsButton]
+                        'attachment': productDetailsAttachment,
+                        'quick_replies': [backToMainMenuButton, showCatalogButton]
                     });
-
-                  });
+                });
             }
-
-        } else if (message.payload == 'start_button_clicked' || message.payload == 'main_menu') {
-        
-            showMainMenu(bot, message);
-
-        } else if (~message.payload.indexOf('view_more')) {
-
-            console.log(message.payload.slice(message.payload.indexOf(' ') + 1));
-
-            
         }
-    })
+    });
+
+    controller.on('message_received', function(bot, message) {
+
+        if (message.quick_reply) {
+
+            if (message.quick_reply.payload.length === 13 && message.quick_reply.payload[0] === '+') {
+
+                bot.reply(message, {
+                    'text': 'Please share your location for delivery',
+                    'quick_replies': [
+                        {
+                            'content_type': 'location'
+                        }
+                    ]
+                });            
+            }
+        } 
+    });
+
+    controller.on('message_received', function(bot, message) {
+
+        if (message.attachments && message.attachments[0].type == 'location') {
+
+            customersLatitude = message.attachments[0].payload.coordinates.lat;
+            customersLongitude = message.attachments[0].payload.coordinates.long;
+
+            console.log(`latitude: ${customersLatitude} longitude: ${customersLongitude}`);
+
+            bot.reply(message, {
+                'text': 'Congratulations! Our courier will contact you within 2 hours',
+                'quick_replies': [backToMainMenuButton, showCatalogButton]
+            });           
+        } 
+    });   
+    
+    
+    function showingCatalog(bot, message, page) {
+
+        const pageNumber = page || 1;
+
+        bby.products('search=digital camera canon', {
+            'format': 'json',
+            'show': 'name,image,shortDescription,sku,salePrice',
+            'page': `${pageNumber}`
+          }, function (err, data) {
+            
+            if (err) {
+                console.warn(err);
+            }
+            
+            const createProductsListAttachment = function(data) {
+                
+                let i;
+                const objectToCreate = {};
+
+                objectToCreate.type = 'template';
+                objectToCreate.payload = {};
+                objectToCreate.payload.template_type = 'generic';
+                objectToCreate.payload.elements = [];
+    
+                for (i = 0; i < 9; i++) {
+                    
+                    if (data.products[i]) {
+    
+                        const productName = data.products[i].name;
+                        const productImageURL = data.products[i].image;
+    
+                        objectToCreate.payload.elements[i] = {};
+                        objectToCreate.payload.elements[i].title = productName;
+                        objectToCreate.payload.elements[i].image_url = productImageURL;
+                        objectToCreate.payload.elements[i].buttons = [
+                            {
+                                'type':'postback',
+                                'title':'View more',
+                                'payload':`view_more ${data.products[i].sku}`,
+                            },
+                            {
+                                'type':'postback',
+                                'title':'Add to favourites',
+                                'payload':`add_to_favourites ${data.products[i].sku}`,
+                            },
+                            {
+                                'type':'postback',
+                                'title':`Buy for ${data.products[i].salePrice} $`,
+                                'payload':`buy_product ${data.products[i].sku}`,
+                            }
+                        ];
+                    }
+                }
+    
+                return objectToCreate;
+            };
+            
+            const productsListAttachment = createProductsListAttachment(data);    
+    
+            if (pageNumber == 1) {
+
+                bot.reply(message, {
+                    "attachment": productsListAttachment,
+                    "quick_replies": [backToMainMenuButton,
+                    {
+                        'content_type': 'text',
+                        'title': 'See more products',
+                        'payload': `go_to_next_page ${pageNumber}`,
+                    }]
+                });
+
+            } else {
+
+                bot.reply(message, {
+                    "attachment": productsListAttachment,
+                    "quick_replies": [
+                        {
+                            'content_type': 'text',
+                            'title': 'See previous page',
+                            'payload': `go_to_previous_page ${pageNumber}`,
+                        },
+                        backToMainMenuButton,
+                        {
+                            'content_type': 'text',
+                            'title': 'See next page',
+                            'payload': `go_to_next_page ${pageNumber}`,
+                        }
+                    ]
+                });
+            }
+    
+        });
+    }
         
 };
 
