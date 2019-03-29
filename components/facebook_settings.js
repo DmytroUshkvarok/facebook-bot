5e3023e4e5e61d04ba59a25ed3ea4d381b6a1a01
ushkvarokdmitriy@gmail.com <ushkvarokdmitriy@gmail.com>
2019-03-29 17:29:12 +0200

    changing bot for african country

--- components/facebook_settings.js
+++ components/facebook_settings.js
@@ -2,7 +2,7 @@ const debug = require('debug')('botkit:facebook_settings')
 
 module.exports = function (controller) {
   debug('Configuring Facebook thread settings...')
-  controller.api.thread_settings.greeting('Look to Up and enjoy. More.')
+  controller.api.thread_settings.greeting('Do you want to always have supply of food and save money on food purchases? No problem. I will help you with this :)')
   controller.api.thread_settings.get_started('start_button_clicked')
   controller.api.thread_settings.menu([
     {
--- components/write_favourites.js
+++ components/write_favourites.js
@@ -1,4 +1,4 @@
-const Customer = require('./db/customer_schema')
+// const Customer = require('./db/customer_schema')
 const mainMenuButton = {
   'content_type': 'text',
   'title': 'To main menu',
@@ -9,55 +9,64 @@ const shopButton = {
   'title': 'Go to shop',
   'payload': 'shop'
 }
+// const favouritesButton = {
+//   'content_type': 'text',
+//   'title': 'To favourites',
+//   'payload': 'favourites'
+// }
 const favouritesButton = {
   'content_type': 'text',
-  'title': 'To favourites',
+  'title': 'My basket',
   'payload': 'favourites'
 }
 
 module.exports = function (bot, message) {
-  const msgId = message.sender.id
-  const payloadFavouriteSKU = message.payload.split(' ')[1]
-  Customer.findOne({ messenger_id: `${msgId}` }).exec(function (err, customer) {
-    if (err) return console.log(err)
-    if (!customer) {
-      console.log(`No have customers with id ${msgId} in base. Adding new customer in base.`)
-      Customer.create({ messenger_id: `${msgId}`, favourites: [`${payloadFavouriteSKU}`] })
-      console.log(`New customer was successfully added to base.`)
-      return bot.reply(message, {
-        'text': 'New customer profile was created. This product was added to your favourites',
-        'quick_replies': [mainMenuButton, shopButton, favouritesButton]
-      })
-    } else {
-      if (!customer.favourites) {
-        console.log(`This customer have no products added to favourites. Adding first product.`)
-        customer.favourites = [`${payloadFavouriteSKU}`]
-        return customer.save(function (err) {
-          if (err) return console.log(err)
-          console.log(`Customers favourites were successfully created with one product.`)
-          return bot.reply(message, {
-            'text': 'Added to favourites',
-            'quick_replies': [mainMenuButton, shopButton, favouritesButton]
-          })
-        })
-      } else {
-        if (~customer.favourites.indexOf(`${payloadFavouriteSKU}`)) {
-          return bot.reply(message, {
-            'text': `This product is already in favourites. have no reason to add one more.`,
-            'quick_replies': [mainMenuButton, shopButton, favouritesButton]
-          })
-        } else {
-          customer.favourites.push(`${payloadFavouriteSKU}`)
-          return customer.save(function (err) {
-            if (err) return console.log(err)
-            console.log(`Customers favourites were successfully updated.`)
-            return bot.reply(message, {
-              'text': 'Added to favourites',
-              'quick_replies': [mainMenuButton, shopButton, favouritesButton]
-            })
-          })
-        }
-      }
-    }
+  // const msgId = message.sender.id
+  // const payloadFavouriteSKU = message.payload.split(' ')[1]
+  // Customer.findOne({ messenger_id: `${msgId}` }).exec(function (err, customer) {
+  //   if (err) return console.log(err)
+  //   if (!customer) {
+  //     console.log(`No have customers with id ${msgId} in base. Adding new customer in base.`)
+  //     Customer.create({ messenger_id: `${msgId}`, favourites: [`${payloadFavouriteSKU}`] })
+  //     console.log(`New customer was successfully added to base.`)
+  //     return bot.reply(message, {
+  //       'text': 'New customer profile was created. This product was added to your favourites',
+  //       'quick_replies': [mainMenuButton, shopButton, favouritesButton]
+  //     })
+  //   } else {
+  //     if (!customer.favourites) {
+  //       console.log(`This customer have no products added to favourites. Adding first product.`)
+  //       customer.favourites = [`${payloadFavouriteSKU}`]
+  //       return customer.save(function (err) {
+  //         if (err) return console.log(err)
+  //         console.log(`Customers favourites were successfully created with one product.`)
+  //         return bot.reply(message, {
+  //           'text': 'Added to favourites',
+  //           'quick_replies': [mainMenuButton, shopButton, favouritesButton]
+  //         })
+  //       })
+  //     } else {
+  //       if (~customer.favourites.indexOf(`${payloadFavouriteSKU}`)) {
+  //         return bot.reply(message, {
+  //           'text': `This product is already in favourites. have no reason to add one more.`,
+  //           'quick_replies': [mainMenuButton, shopButton, favouritesButton]
+  //         })
+  //       } else {
+  //         customer.favourites.push(`${payloadFavouriteSKU}`)
+  //         return customer.save(function (err) {
+  //           if (err) return console.log(err)
+  //           console.log(`Customers favourites were successfully updated.`)
+  //           return bot.reply(message, {
+  //             'text': 'Added to favourites',
+  //             'quick_replies': [mainMenuButton, shopButton, favouritesButton]
+  //           })
+  //         })
+  //       }
+  //     }
+  //   }
+  // })
+  bot.reply(message, {
+    'text': 'Product was added to your basket',
+    'quick_replies': [mainMenuButton, shopButton, favouritesButton]
   })
 }
